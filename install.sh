89f114b213daf5065d4d208de95dead1a0ca2bcd
Kanishk Pachauri <itskanishkp.py@gmail.com>
2024-09-30 16:03:08 +0530

    Update Install Script

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/usr/bin/env sh
+
 set -e
 
 # Determine OS and architecture
@@ -6,35 +7,52 @@ OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 ARCH=$(uname -m)
 
 case $ARCH in
-    x86_64)
-        ARCH="amd64"
-        ;;
-    aarch64)
-        ARCH="arm64"
-        ;;
-    i386|i686)
-        ARCH="386"
-        ;;
+	x86_64)
+		ARCH="amd64"
+		;;
+	aarch64|arm64)
+		ARCH="arm64"
+		;;
+	i386|i686)
+		ARCH="386"
+		;;
+	*)
+		echo "Unsupported architecture: $ARCH"
+		exit 1
+		;;
 esac
 
-# Set version
-VERSION="0.0.1"
+# Check for version argument
+# Otherwise, set latest release version
+if [ -z "$VERSION" ]; then
+	VERSION=$(curl -s "https://api.github.com/repos/fOSS-Community/wand/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([0-9.]+)".*/\1/')
+fi
 
 # Construct download URL
-DOWNLOAD_URL="https://github.com/FOSS-Community/wand/releases/download/v0.0.1/wand_${VERSION}_${OS}_${ARCH}.tar.gz"
+DOWNLOAD_URL="https://github.com/fOSS-Community/wand/releases/download/v${VERSION}/wand_${VERSION}_${OS}_${ARCH}.tar.gz"
 
 # Create temporary directory
 TMP_DIR=$(mktemp -d)
 trap 'rm -rf "$TMP_DIR"' EXIT
 
 # Download and extract
-echo "Downloading Wand ${VERSION} for ${OS} ${ARCH}..."
-curl -L "$DOWNLOAD_URL" | tar -xz -C "$TMP_DIR"
+echo "Downloading wand ${VERSION} for ${OS} ${ARCH}..."
+curl -L -s "$DOWNLOAD_URL" | tar -xz -C "$TMP_DIR"
 
-# Install
-INSTALL_DIR="/usr/local/bin"
-echo "Installing Wand to $INSTALL_DIR..."
-sudo mv "$TMP_DIR/wand" "$INSTALL_DIR/"
+# Check if directory is writable by the current user
+# If not, use sudo to install
+INSTALL_DIR=${INSTALL_DIR:-/usr/local/bin}
+if [ ! -w "$INSTALL_DIR" ]; then
+	echo "Installing wand to $INSTALL_DIR..."
+	sudo mkdir -p "$INSTALL_DIR"
+	sudo mv "$TMP_DIR/wand" "$INSTALL_DIR/"
+else
+	echo "Installing wand to $INSTALL_DIR..."
+	mkdir -p "$INSTALL_DIR"
+	mv "$TMP_DIR/wand" "$INSTALL_DIR/"
+fi
 
-echo "Wand ${VERSION} has been installed successfully!"
+echo "wand ${VERSION} has been installed successfully!"
+echo "Be sure that $INSTALL_DIR is in your PATH:"
+printf "\n\texport PATH=\$PATH:%s\n\n" "$INSTALL_DIR"
 echo "You can now use the 'wand' command."
